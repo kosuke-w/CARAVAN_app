66f6f302347d074bdae9a505b3f3c9376ced9fca
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-06-15 08:43:14 +0000

    [Start]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,7 +26,10 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end 
+  end
+  
+  def destroy
+  end
   
   private
   def blog_params
