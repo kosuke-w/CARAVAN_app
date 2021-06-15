ad3ecbc76d20cf6bec4c659004dd66f37691c792
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-06-15 07:57:31 +0000

    [Finish]3-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
+  
   def index
+    
   end
 
   def show
@@ -10,8 +12,17 @@ class BlogsController < ApplicationController
   end
   
   def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end 
 
   def edit
   end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end 
+  
 end
